3c77c11e67d39ea5f639e6da74d22a8fd3b78776
IuraCPersonal <iurie.cius.personal@gmail.com>
2023-04-11 02:24:43 -0700

    feat(style): added Transparency option

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,6 +2,14 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
+  "sainnhe/everforest",
+  {
+    "xiyaowong/transparent.nvim",
+    event = "BufRead",
+    config = function()
+      require("transparent").setup()
+    end,
+  }
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
